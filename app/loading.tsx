94dd51225fcb30af224e4a8e84e4fa60d485aabf
Rafly Irham Safatulloh <raflyirham1005@gmail.com>
2025-08-04 14:43:20 +0700

    minor update

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center h-screen z-[9999]">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+    <div className="flex justify-center items-center h-dvh z-[9999] bg-black">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500"></div>
     </div>
   );
 }
